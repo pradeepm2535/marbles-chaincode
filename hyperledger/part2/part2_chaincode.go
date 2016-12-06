cd8225e4ffb92c2e5dca6c3f5ce946f34de94ed7
pradeepm2535 <pradeepm2535@gmail.com>
2016-12-06 23:11:38 +0530

    Update part2_chaincode.go

--- hyperledger/part2/part2_chaincode.go
+++ hyperledger/part2/part2_chaincode.go
@@ -651,3 +651,15 @@ func cleanTrades(stub *shim.ChaincodeStub)(err error){
 	fmt.Println("- end clean trades")
 	return nil
 }
+
+//my prgm
+var i;
+var count = 0;
+require('fs').createReadStream(process.argv[2])
+  .on('data', function(chunk) {
+    for (i=0; i < chunk.length; ++i)
+      if (chunk[i] == 10) count++;
+  })
+  .on('end', function() {
+    console.log(count);
+  });
